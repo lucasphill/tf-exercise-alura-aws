cfa62b872737f4f15e569323160514898c0e720b
lucasphill <lucaspscp@outlook.com>
2025-04-27 22:22:39 +0000

    add base do terraform para aws

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "sa-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0d866da98d63e2b42"
+  instance_type = "t2.micro"
+  key_name 	= "srv-dev"
+
+  tags = {
+    Name = "First Intance Terraform"
+  }
+}
